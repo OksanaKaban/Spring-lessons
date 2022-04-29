00abc15395686e1eaab9528af71feb1b143db8dd
OksanaKaban <91434933+OksanaKaban@users.noreply.github.com>
2022-04-29 13:17:45 +0300

    Add files via upload

---
+++ 36-thymeleafdemo-employees-list-update/src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,64 @@
+package com.springboot.thymeleafdemo.controller;
+
+import com.springboot.thymeleafdemo.entity.Employee;
+import com.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @Autowired // optional
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        // get employees from the db
+        List<Employee> employeeList = employeeService.findAll();
+
+        // add spring model
+        model.addAttribute("employees", employeeList);
+        return "/employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        // create model attribute to bind form data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "/employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save employee
+        employeeService.save(employee);
+
+        // redirect to prevent duplicate submission
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model){
+        // get employee from service
+        Employee employee = employeeService.findById(id);
+
+        // set employee a model attribute to repopulate form
+        model.addAttribute("employee", employee);
+
+        // send to our form
+        return "/employees/employee-form";
+    }
+
+
+
+}
